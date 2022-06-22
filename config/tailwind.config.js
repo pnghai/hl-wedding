ac2a65e5f45c98c9719b8410c4ebd32a3dcf4001
hai <pnghai@gmail.com>
2022-06-22 11:11:34 +0700

    update packages

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -67,7 +67,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/forms"),
     require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
     require("daisyui"),
   ]
 };
