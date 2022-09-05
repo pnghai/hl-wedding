4baa416386275d3a6d6abf48f667abd32888fb8b
hai <pnghai@gmail.com>
2022-09-05 19:55:00 +0700

    add favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :rsvps
   root "home#index"
+
+  get 'favicon/webmanifest' => 'favicon#webmanifest', format: :json
+  get 'favicon/browserconfig' => 'favicon#browserconfig', format: :xml
   scope "(:locale)", locale: /en|ja|vi/ do
-    resources :books
+    resources :rsvps, only: [:create]
   end
 end
