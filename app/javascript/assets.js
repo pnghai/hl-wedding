cd329b83c138077f901d0dfb85e3e0107602cc7e
hai <pnghai@gmail.com>
2022-10-12 23:04:43 +0700

    fix photos

--- app/javascript/assets.js
+++ app/javascript/assets.js
@@ -36,6 +36,222 @@ import IMG_5707 from './images/gallery/IMG_5707-Enhanced.jpg';
 import IMG_5830 from './images/gallery/IMG_5830-Enhanced.jpg';
 import IMG_6130 from './images/gallery/IMG_6130-Enhanced.jpg';
 import IMG_6516 from './images/gallery/IMG_6516-Enhanced.jpg';
+import IMG_4768_1080 from './images/gallery/1080/IMG_4768.jpg';
+import IMG_4843_1080 from './images/gallery/1080/IMG_4843-Enhanced.jpg';
+import IMG_5262_1080 from './images/gallery/1080/IMG_5262-Enhanced.jpg';
+import IMG_5290_1080 from './images/gallery/1080/IMG_5290-Enhanced.jpg';
+import IMG_5413_1080 from './images/gallery/1080/IMG_5413-Enhanced.jpg';
+import IMG_5572_1080 from './images/gallery/1080/IMG_5572-Enhanced.jpg';
+import IMG_5732_1080 from './images/gallery/1080/IMG_5732-Enhanced.jpg';
+import IMG_5860_1080 from './images/gallery/1080/IMG_5860-Enhanced.jpg';
+import IMG_6134_1080 from './images/gallery/1080/IMG_6134-Enhanced.jpg';
+import IMG_7382_1080 from './images/gallery/1080/IMG_7382-Enhanced.jpg';
+import IMG_4915_1080 from './images/gallery/1080/IMG_4915-Enhanced.jpg';
+import IMG_5272_1080 from './images/gallery/1080/IMG_5272-Enhanced.jpg';
+import IMG_5336_1080 from './images/gallery/1080/IMG_5336-Enhanced.jpg';
+import IMG_5463_1080 from './images/gallery/1080/IMG_5463-Enhanced.jpg';
+import IMG_5703_1080 from './images/gallery/1080/IMG_5703-Enhanced.jpg';
+import IMG_5766_1080 from './images/gallery/1080/IMG_5766-Enhanced.jpg';
+import IMG_6045_1080 from './images/gallery/1080/IMG_6045-Enhanced.jpg';
+import IMG_6268_1080 from './images/gallery/1080/IMG_6268-Enhanced.jpg';
+import IMG_7401_1080 from './images/gallery/1080/IMG_7401-Enhanced.jpg';
+import IMG_5147_1080 from './images/gallery/1080/IMG_5147-Enhanced.jpg';
+import IMG_5282_1080 from './images/gallery/1080/IMG_5282-Enhanced.jpg';
+import IMG_5411_1080 from './images/gallery/1080/IMG_5411-Enhanced.jpg';
+import IMG_5508_1080 from './images/gallery/1080/IMG_5508-Enhanced.jpg';
+import IMG_5707_1080 from './images/gallery/1080/IMG_5707-Enhanced.jpg';
+import IMG_5830_1080 from './images/gallery/1080/IMG_5830-Enhanced.jpg';
+import IMG_6130_1080 from './images/gallery/1080/IMG_6130-Enhanced.jpg';
+import IMG_6516_1080 from './images/gallery/1080/IMG_6516-Enhanced.jpg';
+import IMG_4768_640 from './images/gallery/640/IMG_4768.jpg';
+import IMG_4843_640 from './images/gallery/640/IMG_4843-Enhanced.jpg';
+import IMG_5262_640 from './images/gallery/640/IMG_5262-Enhanced.jpg';
+import IMG_5290_640 from './images/gallery/640/IMG_5290-Enhanced.jpg';
+import IMG_5413_640 from './images/gallery/640/IMG_5413-Enhanced.jpg';
+import IMG_5572_640 from './images/gallery/640/IMG_5572-Enhanced.jpg';
+import IMG_5732_640 from './images/gallery/640/IMG_5732-Enhanced.jpg';
+import IMG_5860_640 from './images/gallery/640/IMG_5860-Enhanced.jpg';
+import IMG_6134_640 from './images/gallery/640/IMG_6134-Enhanced.jpg';
+import IMG_7382_640 from './images/gallery/640/IMG_7382-Enhanced.jpg';
+import IMG_4915_640 from './images/gallery/640/IMG_4915-Enhanced.jpg';
+import IMG_5272_640 from './images/gallery/640/IMG_5272-Enhanced.jpg';
+import IMG_5336_640 from './images/gallery/640/IMG_5336-Enhanced.jpg';
+import IMG_5463_640 from './images/gallery/640/IMG_5463-Enhanced.jpg';
+import IMG_5703_640 from './images/gallery/640/IMG_5703-Enhanced.jpg';
+import IMG_5766_640 from './images/gallery/640/IMG_5766-Enhanced.jpg';
+import IMG_6045_640 from './images/gallery/640/IMG_6045-Enhanced.jpg';
+import IMG_6268_640 from './images/gallery/640/IMG_6268-Enhanced.jpg';
+import IMG_7401_640 from './images/gallery/640/IMG_7401-Enhanced.jpg';
+import IMG_5147_640 from './images/gallery/640/IMG_5147-Enhanced.jpg';
+import IMG_5282_640 from './images/gallery/640/IMG_5282-Enhanced.jpg';
+import IMG_5411_640 from './images/gallery/640/IMG_5411-Enhanced.jpg';
+import IMG_5508_640 from './images/gallery/640/IMG_5508-Enhanced.jpg';
+import IMG_5707_640 from './images/gallery/640/IMG_5707-Enhanced.jpg';
+import IMG_5830_640 from './images/gallery/640/IMG_5830-Enhanced.jpg';
+import IMG_6130_640 from './images/gallery/640/IMG_6130-Enhanced.jpg';
+import IMG_6516_640 from './images/gallery/640/IMG_6516-Enhanced.jpg';
+import IMG_4768_384 from './images/gallery/384/IMG_4768.jpg';
+import IMG_4843_384 from './images/gallery/384/IMG_4843-Enhanced.jpg';
+import IMG_5262_384 from './images/gallery/384/IMG_5262-Enhanced.jpg';
+import IMG_5290_384 from './images/gallery/384/IMG_5290-Enhanced.jpg';
+import IMG_5413_384 from './images/gallery/384/IMG_5413-Enhanced.jpg';
+import IMG_5572_384 from './images/gallery/384/IMG_5572-Enhanced.jpg';
+import IMG_5732_384 from './images/gallery/384/IMG_5732-Enhanced.jpg';
+import IMG_5860_384 from './images/gallery/384/IMG_5860-Enhanced.jpg';
+import IMG_6134_384 from './images/gallery/384/IMG_6134-Enhanced.jpg';
+import IMG_7382_384 from './images/gallery/384/IMG_7382-Enhanced.jpg';
+import IMG_4915_384 from './images/gallery/384/IMG_4915-Enhanced.jpg';
+import IMG_5272_384 from './images/gallery/384/IMG_5272-Enhanced.jpg';
+import IMG_5336_384 from './images/gallery/384/IMG_5336-Enhanced.jpg';
+import IMG_5463_384 from './images/gallery/384/IMG_5463-Enhanced.jpg';
+import IMG_5703_384 from './images/gallery/384/IMG_5703-Enhanced.jpg';
+import IMG_5766_384 from './images/gallery/384/IMG_5766-Enhanced.jpg';
+import IMG_6045_384 from './images/gallery/384/IMG_6045-Enhanced.jpg';
+import IMG_6268_384 from './images/gallery/384/IMG_6268-Enhanced.jpg';
+import IMG_7401_384 from './images/gallery/384/IMG_7401-Enhanced.jpg';
+import IMG_5147_384 from './images/gallery/384/IMG_5147-Enhanced.jpg';
+import IMG_5282_384 from './images/gallery/384/IMG_5282-Enhanced.jpg';
+import IMG_5411_384 from './images/gallery/384/IMG_5411-Enhanced.jpg';
+import IMG_5508_384 from './images/gallery/384/IMG_5508-Enhanced.jpg';
+import IMG_5707_384 from './images/gallery/384/IMG_5707-Enhanced.jpg';
+import IMG_5830_384 from './images/gallery/384/IMG_5830-Enhanced.jpg';
+import IMG_6130_384 from './images/gallery/384/IMG_6130-Enhanced.jpg';
+import IMG_6516_384 from './images/gallery/384/IMG_6516-Enhanced.jpg';
+import IMG_4768_256 from './images/gallery/256/IMG_4768.jpg';
+import IMG_4843_256 from './images/gallery/256/IMG_4843-Enhanced.jpg';
+import IMG_5262_256 from './images/gallery/256/IMG_5262-Enhanced.jpg';
+import IMG_5290_256 from './images/gallery/256/IMG_5290-Enhanced.jpg';
+import IMG_5413_256 from './images/gallery/256/IMG_5413-Enhanced.jpg';
+import IMG_5572_256 from './images/gallery/256/IMG_5572-Enhanced.jpg';
+import IMG_5732_256 from './images/gallery/256/IMG_5732-Enhanced.jpg';
+import IMG_5860_256 from './images/gallery/256/IMG_5860-Enhanced.jpg';
+import IMG_6134_256 from './images/gallery/256/IMG_6134-Enhanced.jpg';
+import IMG_7382_256 from './images/gallery/256/IMG_7382-Enhanced.jpg';
+import IMG_4915_256 from './images/gallery/256/IMG_4915-Enhanced.jpg';
+import IMG_5272_256 from './images/gallery/256/IMG_5272-Enhanced.jpg';
+import IMG_5336_256 from './images/gallery/256/IMG_5336-Enhanced.jpg';
+import IMG_5463_256 from './images/gallery/256/IMG_5463-Enhanced.jpg';
+import IMG_5703_256 from './images/gallery/256/IMG_5703-Enhanced.jpg';
+import IMG_5766_256 from './images/gallery/256/IMG_5766-Enhanced.jpg';
+import IMG_6045_256 from './images/gallery/256/IMG_6045-Enhanced.jpg';
+import IMG_6268_256 from './images/gallery/256/IMG_6268-Enhanced.jpg';
+import IMG_7401_256 from './images/gallery/256/IMG_7401-Enhanced.jpg';
+import IMG_5147_256 from './images/gallery/256/IMG_5147-Enhanced.jpg';
+import IMG_5282_256 from './images/gallery/256/IMG_5282-Enhanced.jpg';
+import IMG_5411_256 from './images/gallery/256/IMG_5411-Enhanced.jpg';
+import IMG_5508_256 from './images/gallery/256/IMG_5508-Enhanced.jpg';
+import IMG_5707_256 from './images/gallery/256/IMG_5707-Enhanced.jpg';
+import IMG_5830_256 from './images/gallery/256/IMG_5830-Enhanced.jpg';
+import IMG_6130_256 from './images/gallery/256/IMG_6130-Enhanced.jpg';
+import IMG_6516_256 from './images/gallery/256/IMG_6516-Enhanced.jpg';
+import IMG_4768_128 from './images/gallery/128/IMG_4768.jpg';
+import IMG_4843_128 from './images/gallery/128/IMG_4843-Enhanced.jpg';
+import IMG_5262_128 from './images/gallery/128/IMG_5262-Enhanced.jpg';
+import IMG_5290_128 from './images/gallery/128/IMG_5290-Enhanced.jpg';
+import IMG_5413_128 from './images/gallery/128/IMG_5413-Enhanced.jpg';
+import IMG_5572_128 from './images/gallery/128/IMG_5572-Enhanced.jpg';
+import IMG_5732_128 from './images/gallery/128/IMG_5732-Enhanced.jpg';
+import IMG_5860_128 from './images/gallery/128/IMG_5860-Enhanced.jpg';
+import IMG_6134_128 from './images/gallery/128/IMG_6134-Enhanced.jpg';
+import IMG_7382_128 from './images/gallery/128/IMG_7382-Enhanced.jpg';
+import IMG_4915_128 from './images/gallery/128/IMG_4915-Enhanced.jpg';
+import IMG_5272_128 from './images/gallery/128/IMG_5272-Enhanced.jpg';
+import IMG_5336_128 from './images/gallery/128/IMG_5336-Enhanced.jpg';
+import IMG_5463_128 from './images/gallery/128/IMG_5463-Enhanced.jpg';
+import IMG_5703_128 from './images/gallery/128/IMG_5703-Enhanced.jpg';
+import IMG_5766_128 from './images/gallery/128/IMG_5766-Enhanced.jpg';
+import IMG_6045_128 from './images/gallery/128/IMG_6045-Enhanced.jpg';
+import IMG_6268_128 from './images/gallery/128/IMG_6268-Enhanced.jpg';
+import IMG_7401_128 from './images/gallery/128/IMG_7401-Enhanced.jpg';
+import IMG_5147_128 from './images/gallery/128/IMG_5147-Enhanced.jpg';
+import IMG_5282_128 from './images/gallery/128/IMG_5282-Enhanced.jpg';
+import IMG_5411_128 from './images/gallery/128/IMG_5411-Enhanced.jpg';
+import IMG_5508_128 from './images/gallery/128/IMG_5508-Enhanced.jpg';
+import IMG_5707_128 from './images/gallery/128/IMG_5707-Enhanced.jpg';
+import IMG_5830_128 from './images/gallery/128/IMG_5830-Enhanced.jpg';
+import IMG_6130_128 from './images/gallery/128/IMG_6130-Enhanced.jpg';
+import IMG_6516_128 from './images/gallery/128/IMG_6516-Enhanced.jpg';
+import IMG_4768_96 from './images/gallery/96/IMG_4768.jpg';
+import IMG_4843_96 from './images/gallery/96/IMG_4843-Enhanced.jpg';
+import IMG_5262_96 from './images/gallery/96/IMG_5262-Enhanced.jpg';
+import IMG_5290_96 from './images/gallery/96/IMG_5290-Enhanced.jpg';
+import IMG_5413_96 from './images/gallery/96/IMG_5413-Enhanced.jpg';
+import IMG_5572_96 from './images/gallery/96/IMG_5572-Enhanced.jpg';
+import IMG_5732_96 from './images/gallery/96/IMG_5732-Enhanced.jpg';
+import IMG_5860_96 from './images/gallery/96/IMG_5860-Enhanced.jpg';
+import IMG_6134_96 from './images/gallery/96/IMG_6134-Enhanced.jpg';
+import IMG_7382_96 from './images/gallery/96/IMG_7382-Enhanced.jpg';
+import IMG_4915_96 from './images/gallery/96/IMG_4915-Enhanced.jpg';
+import IMG_5272_96 from './images/gallery/96/IMG_5272-Enhanced.jpg';
+import IMG_5336_96 from './images/gallery/96/IMG_5336-Enhanced.jpg';
+import IMG_5463_96 from './images/gallery/96/IMG_5463-Enhanced.jpg';
+import IMG_5703_96 from './images/gallery/96/IMG_5703-Enhanced.jpg';
+import IMG_5766_96 from './images/gallery/96/IMG_5766-Enhanced.jpg';
+import IMG_6045_96 from './images/gallery/96/IMG_6045-Enhanced.jpg';
+import IMG_6268_96 from './images/gallery/96/IMG_6268-Enhanced.jpg';
+import IMG_7401_96 from './images/gallery/96/IMG_7401-Enhanced.jpg';
+import IMG_5147_96 from './images/gallery/96/IMG_5147-Enhanced.jpg';
+import IMG_5282_96 from './images/gallery/96/IMG_5282-Enhanced.jpg';
+import IMG_5411_96 from './images/gallery/96/IMG_5411-Enhanced.jpg';
+import IMG_5508_96 from './images/gallery/96/IMG_5508-Enhanced.jpg';
+import IMG_5707_96 from './images/gallery/96/IMG_5707-Enhanced.jpg';
+import IMG_5830_96 from './images/gallery/96/IMG_5830-Enhanced.jpg';
+import IMG_6130_96 from './images/gallery/96/IMG_6130-Enhanced.jpg';
+import IMG_6516_96 from './images/gallery/96/IMG_6516-Enhanced.jpg';
+import IMG_4768_48 from './images/gallery/48/IMG_4768.jpg';
+import IMG_4843_48 from './images/gallery/48/IMG_4843-Enhanced.jpg';
+import IMG_5262_48 from './images/gallery/48/IMG_5262-Enhanced.jpg';
+import IMG_5290_48 from './images/gallery/48/IMG_5290-Enhanced.jpg';
+import IMG_5413_48 from './images/gallery/48/IMG_5413-Enhanced.jpg';
+import IMG_5572_48 from './images/gallery/48/IMG_5572-Enhanced.jpg';
+import IMG_5732_48 from './images/gallery/48/IMG_5732-Enhanced.jpg';
+import IMG_5860_48 from './images/gallery/48/IMG_5860-Enhanced.jpg';
+import IMG_6134_48 from './images/gallery/48/IMG_6134-Enhanced.jpg';
+import IMG_7382_48 from './images/gallery/48/IMG_7382-Enhanced.jpg';
+import IMG_4915_48 from './images/gallery/48/IMG_4915-Enhanced.jpg';
+import IMG_5272_48 from './images/gallery/48/IMG_5272-Enhanced.jpg';
+import IMG_5336_48 from './images/gallery/48/IMG_5336-Enhanced.jpg';
+import IMG_5463_48 from './images/gallery/48/IMG_5463-Enhanced.jpg';
+import IMG_5703_48 from './images/gallery/48/IMG_5703-Enhanced.jpg';
+import IMG_5766_48 from './images/gallery/48/IMG_5766-Enhanced.jpg';
+import IMG_6045_48 from './images/gallery/48/IMG_6045-Enhanced.jpg';
+import IMG_6268_48 from './images/gallery/48/IMG_6268-Enhanced.jpg';
+import IMG_7401_48 from './images/gallery/48/IMG_7401-Enhanced.jpg';
+import IMG_5147_48 from './images/gallery/48/IMG_5147-Enhanced.jpg';
+import IMG_5282_48 from './images/gallery/48/IMG_5282-Enhanced.jpg';
+import IMG_5411_48 from './images/gallery/48/IMG_5411-Enhanced.jpg';
+import IMG_5508_48 from './images/gallery/48/IMG_5508-Enhanced.jpg';
+import IMG_5707_48 from './images/gallery/48/IMG_5707-Enhanced.jpg';
+import IMG_5830_48 from './images/gallery/48/IMG_5830-Enhanced.jpg';
+import IMG_6130_48 from './images/gallery/48/IMG_6130-Enhanced.jpg';
+import IMG_6516_48 from './images/gallery/48/IMG_6516-Enhanced.jpg';
+import IMG_4768_2400 from './images/gallery/2400/IMG_4768.jpg';
+import IMG_4843_2400 from './images/gallery/2400/IMG_4843-Enhanced.jpg';
+import IMG_5262_2400 from './images/gallery/2400/IMG_5262-Enhanced.jpg';
+import IMG_5290_2400 from './images/gallery/2400/IMG_5290-Enhanced.jpg';
+import IMG_5413_2400 from './images/gallery/2400/IMG_5413-Enhanced.jpg';
+import IMG_5572_2400 from './images/gallery/2400/IMG_5572-Enhanced.jpg';
+import IMG_5732_2400 from './images/gallery/2400/IMG_5732-Enhanced.jpg';
+import IMG_5860_2400 from './images/gallery/2400/IMG_5860-Enhanced.jpg';
+import IMG_6134_2400 from './images/gallery/2400/IMG_6134-Enhanced.jpg';
+import IMG_7382_2400 from './images/gallery/2400/IMG_7382-Enhanced.jpg';
+import IMG_4915_2400 from './images/gallery/2400/IMG_4915-Enhanced.jpg';
+import IMG_5272_2400 from './images/gallery/2400/IMG_5272-Enhanced.jpg';
+import IMG_5336_2400 from './images/gallery/2400/IMG_5336-Enhanced.jpg';
+import IMG_5463_2400 from './images/gallery/2400/IMG_5463-Enhanced.jpg';
+import IMG_5703_2400 from './images/gallery/2400/IMG_5703-Enhanced.jpg';
+import IMG_5766_2400 from './images/gallery/2400/IMG_5766-Enhanced.jpg';
+import IMG_6045_2400 from './images/gallery/2400/IMG_6045-Enhanced.jpg';
+import IMG_6268_2400 from './images/gallery/2400/IMG_6268-Enhanced.jpg';
+import IMG_7401_2400 from './images/gallery/2400/IMG_7401-Enhanced.jpg';
+import IMG_5147_2400 from './images/gallery/2400/IMG_5147-Enhanced.jpg';
+import IMG_5282_2400 from './images/gallery/2400/IMG_5282-Enhanced.jpg';
+import IMG_5411_2400 from './images/gallery/2400/IMG_5411-Enhanced.jpg';
+import IMG_5508_2400 from './images/gallery/2400/IMG_5508-Enhanced.jpg';
+import IMG_5707_2400 from './images/gallery/2400/IMG_5707-Enhanced.jpg';
+import IMG_5830_2400 from './images/gallery/2400/IMG_5830-Enhanced.jpg';
+import IMG_6130_2400 from './images/gallery/2400/IMG_6130-Enhanced.jpg';
+import IMG_6516_2400 from './images/gallery/2400/IMG_6516-Enhanced.jpg';
 export {
   olive,
   couple,
@@ -75,4 +291,220 @@ export {
   IMG_5830,
   IMG_6130,
   IMG_6516,
+  IMG_4768_1080,
+  IMG_4843_1080,
+  IMG_5262_1080,
+  IMG_5290_1080,
+  IMG_5413_1080,
+  IMG_5572_1080,
+  IMG_5732_1080,
+  IMG_5860_1080,
+  IMG_6134_1080,
+  IMG_7382_1080,
+  IMG_4915_1080,
+  IMG_5272_1080,
+  IMG_5336_1080,
+  IMG_5463_1080,
+  IMG_5703_1080,
+  IMG_5766_1080,
+  IMG_6045_1080,
+  IMG_6268_1080,
+  IMG_7401_1080,
+  IMG_5147_1080,
+  IMG_5282_1080,
+  IMG_5411_1080,
+  IMG_5508_1080,
+  IMG_5707_1080,
+  IMG_5830_1080,
+  IMG_6130_1080,
+  IMG_6516_1080,
+  IMG_4768_640,
+  IMG_4843_640,
+  IMG_5262_640,
+  IMG_5290_640,
+  IMG_5413_640,
+  IMG_5572_640,
+  IMG_5732_640,
+  IMG_5860_640,
+  IMG_6134_640,
+  IMG_7382_640,
+  IMG_4915_640,
+  IMG_5272_640,
+  IMG_5336_640,
+  IMG_5463_640,
+  IMG_5703_640,
+  IMG_5766_640,
+  IMG_6045_640,
+  IMG_6268_640,
+  IMG_7401_640,
+  IMG_5147_640,
+  IMG_5282_640,
+  IMG_5411_640,
+  IMG_5508_640,
+  IMG_5707_640,
+  IMG_5830_640,
+  IMG_6130_640,
+  IMG_6516_640,
+  IMG_4768_384,
+  IMG_4843_384,
+  IMG_5262_384,
+  IMG_5290_384,
+  IMG_5413_384,
+  IMG_5572_384,
+  IMG_5732_384,
+  IMG_5860_384,
+  IMG_6134_384,
+  IMG_7382_384,
+  IMG_4915_384,
+  IMG_5272_384,
+  IMG_5336_384,
+  IMG_5463_384,
+  IMG_5703_384,
+  IMG_5766_384,
+  IMG_6045_384,
+  IMG_6268_384,
+  IMG_7401_384,
+  IMG_5147_384,
+  IMG_5282_384,
+  IMG_5411_384,
+  IMG_5508_384,
+  IMG_5707_384,
+  IMG_5830_384,
+  IMG_6130_384,
+  IMG_6516_384,
+  IMG_4768_256,
+  IMG_4843_256,
+  IMG_5262_256,
+  IMG_5290_256,
+  IMG_5413_256,
+  IMG_5572_256,
+  IMG_5732_256,
+  IMG_5860_256,
+  IMG_6134_256,
+  IMG_7382_256,
+  IMG_4915_256,
+  IMG_5272_256,
+  IMG_5336_256,
+  IMG_5463_256,
+  IMG_5703_256,
+  IMG_5766_256,
+  IMG_6045_256,
+  IMG_6268_256,
+  IMG_7401_256,
+  IMG_5147_256,
+  IMG_5282_256,
+  IMG_5411_256,
+  IMG_5508_256,
+  IMG_5707_256,
+  IMG_5830_256,
+  IMG_6130_256,
+  IMG_6516_256,
+  IMG_4768_128,
+  IMG_4843_128,
+  IMG_5262_128,
+  IMG_5290_128,
+  IMG_5413_128,
+  IMG_5572_128,
+  IMG_5732_128,
+  IMG_5860_128,
+  IMG_6134_128,
+  IMG_7382_128,
+  IMG_4915_128,
+  IMG_5272_128,
+  IMG_5336_128,
+  IMG_5463_128,
+  IMG_5703_128,
+  IMG_5766_128,
+  IMG_6045_128,
+  IMG_6268_128,
+  IMG_7401_128,
+  IMG_5147_128,
+  IMG_5282_128,
+  IMG_5411_128,
+  IMG_5508_128,
+  IMG_5707_128,
+  IMG_5830_128,
+  IMG_6130_128,
+  IMG_6516_128,
+  IMG_4768_96,
+  IMG_4843_96,
+  IMG_5262_96,
+  IMG_5290_96,
+  IMG_5413_96,
+  IMG_5572_96,
+  IMG_5732_96,
+  IMG_5860_96,
+  IMG_6134_96,
+  IMG_7382_96,
+  IMG_4915_96,
+  IMG_5272_96,
+  IMG_5336_96,
+  IMG_5463_96,
+  IMG_5703_96,
+  IMG_5766_96,
+  IMG_6045_96,
+  IMG_6268_96,
+  IMG_7401_96,
+  IMG_5147_96,
+  IMG_5282_96,
+  IMG_5411_96,
+  IMG_5508_96,
+  IMG_5707_96,
+  IMG_5830_96,
+  IMG_6130_96,
+  IMG_6516_96,
+  IMG_4768_48,
+  IMG_4843_48,
+  IMG_5262_48,
+  IMG_5290_48,
+  IMG_5413_48,
+  IMG_5572_48,
+  IMG_5732_48,
+  IMG_5860_48,
+  IMG_6134_48,
+  IMG_7382_48,
+  IMG_4915_48,
+  IMG_5272_48,
+  IMG_5336_48,
+  IMG_5463_48,
+  IMG_5703_48,
+  IMG_5766_48,
+  IMG_6045_48,
+  IMG_6268_48,
+  IMG_7401_48,
+  IMG_5147_48,
+  IMG_5282_48,
+  IMG_5411_48,
+  IMG_5508_48,
+  IMG_5707_48,
+  IMG_5830_48,
+  IMG_6130_48,
+  IMG_6516_48,
+  IMG_4768_2400,
+  IMG_4843_2400,
+  IMG_5262_2400,
+  IMG_5290_2400,
+  IMG_5413_2400,
+  IMG_5572_2400,
+  IMG_5732_2400,
+  IMG_5860_2400,
+  IMG_6134_2400,
+  IMG_7382_2400,
+  IMG_4915_2400,
+  IMG_5272_2400,
+  IMG_5336_2400,
+  IMG_5463_2400,
+  IMG_5703_2400,
+  IMG_5766_2400,
+  IMG_6045_2400,
+  IMG_6268_2400,
+  IMG_7401_2400,
+  IMG_5147_2400,
+  IMG_5282_2400,
+  IMG_5411_2400,
+  IMG_5508_2400,
+  IMG_5707_2400,
+  IMG_5830_2400,
+  IMG_6130_2400,
+  IMG_6516_2400,
 }
--- app/javascript/react/src/i18n.js
+++ app/javascript/react/src/i18n.js
@@ -27,7 +27,7 @@ i18n
           HttpApi               // fallback backend
         ],
         backendOptions: [{
-          defaultVersion: '0.1.10.10',
+          defaultVersion: '0.1.10.11',
           /* options for primary backend */
         }, {
           /* options for secondary backend */
